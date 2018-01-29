18ed2aad33f6edbc85af923c37578871f6572c74
hankai <hankaibj@me.com>
2018-01-29 13:58:25 +0800

    添加分类文件，并修改原文件

--- Category/UIBarButtonItem+HKTExtension.h
+++ Category/UIBarButtonItem+HKTExtension.h
@@ -1,17 +1,45 @@
 //
 //  UIBarButtonItem+HKTExtension.h
-//  TemporaryDemo
+//  VencentDeepPockets
 //
-//  Created by hankai on 2017/11/3.
-//  Copyright © 2017年 Vencent. All rights reserved.
+//  Created by hankai on 2018/1/29.
+//  Copyright © 2018年 Vencent. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface UIBarButtonItem (HKTExtension)
-+(UIBarButtonItem *)itemWithDefaultImgName:(NSString *)defaultImgName highLightImgName:(NSString *)highLightImgName target:(id)target action:(SEL)action;
-+(UIBarButtonItem *)cycloItemWithDefaultImgName:(NSString *)defaultImgName highLightImgName:(NSString *)highLightImgName target:(id)target action:(SEL)action;
 
-+(UIBarButtonItem *)cycloItemWithDefaultImg:(UIImage *)defaultImg highLightImg:(UIImage *)highLightImg target:(id)target action:(SEL)action;
+/**
+ 没有图片调整的按钮
+
+ @param image 正常图
+ @param highImage 高亮图
+ */
++ (instancetype)itemWithImage:(NSString *)image highImage:(NSString *)highImage target:(id)target action:(SEL)action;
+
+
+/**
+ 按钮为圆形
+ 
+ @param image 正常图
+ @param highImage 高亮图
+ */
++(instancetype)cycloItemWithImage:(UIImage *)image highImage:(UIImage *)highImage target:(id)target action:(SEL)action;
+
+
+/**
+ 返回没有调整图片
+ 
+ @param imageName 正常图片名称
+ @param highImageName 高亮图片名称
+ */
++ (instancetype)itemWithImageName:(NSString *)imageName highImageName:(NSString *)highImageName target:target action:(SEL)action;
+
+
+/**
+ 没有文字调整的按钮
+ */
++ (instancetype)itemWithName:(NSString *)Name font:(CGFloat)font backcolor:(UIColor *)backcolor titleColor:(UIColor *)titleColor target:target action:(SEL)action;
 
 @end
--- Category/UIView+HKTExtension.m
+++ Category/UIView+HKTExtension.m
@@ -1,14 +1,15 @@
 //
 //  UIView+HKTExtension.m
-//  TemporaryDemo
+//  VencentDeepPockets
 //
-//  Created by hankai on 2017/10/31.
-//  Copyright © 2017年 Vencent. All rights reserved.
+//  Created by hankai on 2018/1/29.
+//  Copyright © 2018年 Vencent. All rights reserved.
 //
 
 #import "UIView+HKTExtension.h"
 
 @implementation UIView (HKTExtension)
+
 - (void)setSize:(CGSize)size
 {
     CGRect frame = self.frame;
@@ -100,5 +101,39 @@
 -(CGFloat)centerY{
     return self.center.y;
 }
+
+
+
+
+- (void)setBorderColor:(UIColor *)borderColor {
+    self.layer.borderColor = borderColor.CGColor;
+}
+
+- (UIColor *)borderColor {
+    return [UIColor colorWithCGColor:self.layer.borderColor];
+}
+
+- (void)setBorderWidth:(CGFloat)borderWidth {
+    self.layer.borderWidth = borderWidth;
+}
+
+- (CGFloat)borderWidth {
+    return self.layer.borderWidth;
+}
+
+- (void)setCornerRadius:(CGFloat)cornerRadius {
+    self.layer.cornerRadius = cornerRadius;
+    self.layer.masksToBounds = YES;
+    
+    // 栅格化 - 提高性能
+    // 设置栅格化后，图层会被渲染成图片，并且缓存，再次使用时，不会重新渲染
+    self.layer.rasterizationScale = [UIScreen mainScreen].scale;
+    self.layer.shouldRasterize = YES;
+}
+
+- (CGFloat)cornerRadius {
+    return self.layer.cornerRadius;
+}
+
 
 @end
