f096823ea2b59c0b58cff62eef44d5e77bfa82e9
han kai <hankai@zhangyue.com>
2018-04-13 11:01:31 +0800

    修改目录结构，增加DemoProject

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Edward on 2018/4/13.
+//  Copyright © 2018年 HANKAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Example
+//
+//  Created by Edward on 2018/4/13.
+//  Copyright © 2018年 HANKAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
